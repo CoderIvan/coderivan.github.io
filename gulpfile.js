50ac45ac1036720b78cab1edb78ad6a2d6a7e815
Xuehua Huang <xuehua.huang@dbjtech.com>
2015-11-01 10:47:45 +0800

    beta

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
+var child_process = require('child_process')
 var gulp = require('gulp')
 var markdown = require('gulp-markdown')
+var del = require('del')
 
-gulp.task('default', ['markdown_to_html'], function() {
-	gulp.src('public/**')
-		.pipe(gulp.dest('_dist'))
-});
+gulp.task('default', ['clean', 'checkout_markdown', 'markdown_to_html'], function() {
+	gulp.src('app/**').pipe(gulp.dest('_public/'))
+})
+
+gulp.task('clean', function() {
+	del.sync(['_archives', '_public'])
+})
+
+gulp.task('checkout_markdown', function() {
+	child_process.execSync('git clone -b source -l -s . _archives')
+})
 
 gulp.task('markdown_to_html', function() {
-	return gulp.src('./_archives/**')
+	return gulp.src('_archives/**')
 		.pipe(markdown())
-		.pipe(gulp.dest('_dist/html/archives'))
+		.pipe(gulp.dest('_public/views/archives'))
 })
 
 gulp.start()
--- tools/source.js
+++
@@ -1,9 +0,0 @@
-var git = require('gift')
-
-module.exports = function(repo, dest) {
-	return new Promise(function(resolve, reject) {
-		git.clone(repo, dest, function(err, Repo) {
-			err ? reject(err) : resolve(Repo)
-		})
-	})
-}
