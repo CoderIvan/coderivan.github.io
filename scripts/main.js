cc5c7b7ffe38e71566ab71b7ae4626bbe60840e3
Xuehua Huang <xuehua.huang@dbjtech.com>
2015-11-04 09:34:06 +0800

    Update 2015-11-04T01:32:02.999Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,35 @@
-var module = angular.module('myApp', ['ui.router'])
+angular
+	.module('myApp', ['ui.router', 'ngAnimate'])
+	.run(['$rootScope', '$state', function($rootScope, $state) {
+		$rootScope.$state = $state
+	}])
+	.config(function($stateProvider, $urlRouterProvider) {
 
-var archives = [{
-	title: 'AMD_&&_CommonJS'
-}, {
-	title: 'CORS'
-}, {
-	title: 'Docker化方案'
-}]
+		$urlRouterProvider.otherwise('/')
 
-module.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/')
-
-	$stateProvider.state('index', {
-		url: '/',
-		templateUrl: 'views/table_of_content.html',
-		controller: function($scope) {
-			$scope.archives = archives
-		}
-	})
-	for (var i = 0; i < archives.length; i++) {
-		$stateProvider.state(archives[i].title, {
-			url: '/' + archives[i].title,
-			templateUrl: 'views/archives/' + archives[i].title
+		$stateProvider.state('home', {
+			url: '/',
+			template: '<h1>HelloWorld</h1>'
+		}).state('archives', {
+			url: '/archives',
+			templateUrl: 'views/table_of_content.html',
+			controller: function($scope, $http) {
+				$http({
+					method: 'GET',
+					url: 'scripts/archives.json'
+				}).then(function successCallback(response) {
+					$scope.archives = response.data
+				}, function errorCallback(response) {
+					console.error(response)
+				})
+			}
+		}).state('archives.detail', {
+			url: '/:title',
+			templateUrl: function($stateParams) {
+				return 'views/archives/' + $stateParams.title + '.html'
+			}
+		}).state('about', {
+			url: '/about',
+			template: '<h1>About Me</h1>'
 		})
-	}
-})
+	})
